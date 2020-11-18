ec68efe4f2724d0c0ea6884271b6802512298e8c
manvir12012 <singh.manvir06@gmail.com>
2020-11-18 15:25:12 -0500

    minor corrections

--- javascript/main.js
+++ javascript/main.js
@@ -6,7 +6,7 @@ https://www.facebook.com/sharer.php?u=[post-url]
 
 
 Twitter:
-https://twitter.com/share?url=[post-url]&text=[post-title]&via=[via]&hashtags=[hashtags]
+https://twitter.com/share?url=[post-url]&text=[post-title]
 
 
 Linkedin:
@@ -27,9 +27,9 @@ function init() {
     let postURL = encodeURI(document.location.href);
     let postTitle = encodeURI("Hi, please check this out: ");
 
-    facebookBtn.setAttribute("href",`https://www.facebook.com/sharer.php?u=${postURL}`);
+    facebookBtn.setAttribute("href", `https://www.facebook.com/sharer.php?u=${postURL}`);
     twitterBtn.setAttribute("href", `https://twitter.com/share?url=${postURL}&text=${postTitle}`);
-    linkedinBtn.setAttribute("href",`https://www.linkedin.com/shareArticle?url=${postURL}&title=${postTitle}`);
+    linkedinBtn.setAttribute("href", `https://www.linkedin.com/shareArticle?url=${postURL}&title=${postTitle}`);
     whatsappBtn.setAttribute("href", `https://api.whatsapp.com/send?text=${postTitle} ${postURL}`);
 }
 
